5fd4e022f8081f64e7cccebc71645fdd8b0bf63d
majorbonghits420 <jcc384@cornell.edu>
2017-11-06 17:25:54 -0500

    Outline of how URLs will be routed

--- server.cpp
+++ server.cpp
@@ -13,11 +13,27 @@ class AudioServer : public Wt::WApplication {
 public:
   AudioServer(const Wt::WEnvironment &env);
 
+  void routePath(const std::string &interalPath);
+
   static void playVideo(std::string url);
 };
 
 void AudioServer::playVideo(std::string url) {
   system(("cvlc -Vdummy " + url).c_str());
+}
+
+void AudioServer::routePath(const std::string &internalPath) {
+  if (internalPath == "/vote") {
+    // Display voting options
+  } else if (internalPath == "/user") {
+    // Display logged in user information
+  } else if (internalPath == "/login") {
+    // Take user to login page
+  } else if (internalPath == "/submit") {
+    // Take user to lin submission page
+  } else {
+    // If all else fails, take to login page
+  }
 }
 
 int main(int argc, char **argv) {}

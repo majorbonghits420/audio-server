8eea486e26f0987bdc1e2fd5907df8faff973b80
majorbonghits420 <jcc384@cornell.edu>
2017-11-14 01:52:53 -0500

    Fixes and adds to Song.hpp

--- song.hpp
+++ song.hpp
@@ -1,6 +1,17 @@
+#ifndef __SONG_HPP__
+#define __SONG_HPP__
+
 #include <string>
 
 class Song {
+
+public:
+
+  Song()
+    : url("https://www.youtube.com/watch?v=dQw4w9WgXcQ"), // Defualt URL is a rick roll
+      user("Rick Astley"),
+      taste(0.0)
+  {}
 
   Song(std::string url, std::string user)
     : url(url), user(user)
@@ -19,3 +30,5 @@ private:
   std::string user; /**< The username of person who submitted the url*/
   int taste; /**< Taste number for the user who submitted the song */
 };
+
+#endif /* __SONG_HPP__ */

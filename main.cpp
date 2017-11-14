943674602daa0987c282f38a4031f51ffa04d76e
majorbonghits420 <jcc384@cornell.edu>
2017-11-14 15:59:17 -0500

    Add test of audioserver to Main

--- main.cpp
+++ main.cpp
@@ -4,7 +4,29 @@
  */
 
 #include <iostream>
+#include "server.hpp"
+#include "db.hpp"
 
 int main(int argc, char **argv) {
   std::cout << "test" << std::endl;
+  Db db("test.db");
+  db.addUser("james", "password");
+  db.addUser("brendjaun", "isuck");
+  db.updateTaste("brendjuan", false);
+  db.updateTaste("brendjuan", false);
+  db.updateTaste("brendjuan", false);
+  db.updateTaste("brendjuan", false);
+  db.updateTaste("james", true);
+  db.updateTaste("james", true);
+  db.updateTaste("james", true);
+  db.updateTaste("james", true);
+
+  AudioServer server(&db);
+
+  server.addSong("brendjuan","https://www.youtube.com/watch?v=JPVIgshtOag");
+  server.addSong("brendjuan","https://www.youtube.com/watch?v=HHrMkAsGFlE");
+  server.addSong("james", "https://www.youtube.com/watch?v=NT783hZIUXY");
+  server.addSong("james", "https://www.youtube.com/watch?v=ZjajvMxji84");
+
+  server.run();
 }

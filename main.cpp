6fa4c70b859a9c6b3bb09d85dd783fb3ff980147
majorbonghits420 <jcc384@cornell.edu>
2017-11-28 16:58:28 -0500

    Main file starts audio server and starts http server

--- main.cpp
+++ main.cpp
@@ -3,30 +3,41 @@
  * It also does processing on any command line arguments
  */
 
-#include <iostream>
+#include "Globals.hpp"
 #include "server.hpp"
-#include "db.hpp"
+#include "webserver.hpp"
 
-int main(int argc, char **argv) {
-  std::cout << "test" << std::endl;
-  Db db("test.db");
-  db.addUser("james", "password");
-  db.addUser("brendjuan", "isuck");
-  db.updateTaste("brendjuan", false);
-  db.updateTaste("brendjuan", false);
-  db.updateTaste("brendjuan", false);
-  db.updateTaste("brendjuan", false);
-  db.updateTaste("james", true);
-  db.updateTaste("james", true);
-  db.updateTaste("james", true);
-  db.updateTaste("james", true);
+void runServer(AudioServer *server) {
+  server->run(FIFO_NAME);
+}
 
+Wt::WApplication *createApplication(const Wt::WEnvironment& env)
+{
+  /*
+   * You could read information from the environment to decide whether
+   * the user has permission to start a new application
+   */
+  return new Webserver(env);
+}
+
+int main(int argc, char **argv)
+{
+  Db db(DB_NAME);
   AudioServer server(&db);
+  std::cout << "server created" << std::endl;
+  std::thread serverThread(runServer, &server);
+  std::cout << "Started server thread" << std::endl;
 
-  server.addSong("brendjuan","https://www.youtube.com/watch?v=JPVIgshtOag");
-  server.addSong("brendjuan","https://www.youtube.com/watch?v=HHrMkAsGFlE");
-  server.addSong("james", "https://www.youtube.com/watch?v=NT783hZIUXY");
-  server.addSong("james", "https://www.youtube.com/watch?v=ZjajvMxji84");
-
-  server.run();
+  /*
+   * Your main method may set up some shared resources, but should then
+   * start the server application (FastCGI or httpd) that starts listening
+   * for requests, and handles all of the application life cycles.
+   *
+   * The last argument to WRun specifies the function that will instantiate
+   * new application objects. That function is executed when a new user surfs
+   * to the Wt application, and after the library has negotiated browser
+   * support. The function should return a newly instantiated application
+   * object.
+   */
+  return Wt::WRun(argc, argv, &createApplication);
 }
